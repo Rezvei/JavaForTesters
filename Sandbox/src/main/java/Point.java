bacfe2a5e5ec66ad6b72e1850f67ccdd56587cd8
MikhanMikhan <kabelecsup@gmail.com>
2019-11-17 00:29:16 +0700

    add class Point

---
+++ Sandbox/src/main/java/Point.java
@@ -0,0 +1,14 @@
+public class Point {
+
+    double a;
+    double b;
+
+    public Point (double a, double b){
+        this.a=a;
+        this.b=b;
+    }
+
+//    public double distance() {
+//        return this.a-this.b;
+//    }
+}
